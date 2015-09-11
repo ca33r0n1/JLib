86cce2bba90f9d87668055f900462e72ace3a103
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-09-11 17:13:23 +0200

    Updated the ConfigLoader to automatically backup outdated configs.

--- src/com/j0ach1mmall3/jlib/storage/yaml/ConfigLoader.java
+++ src/com/j0ach1mmall3/jlib/storage/yaml/ConfigLoader.java
@@ -1,7 +1,11 @@
 package com.j0ach1mmall3.jlib.storage.yaml;
 
+import com.j0ach1mmall3.jlib.methods.General;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 /**
  * Created by j0ach1mmall3 on 13:46 21/08/2015 using IntelliJ IDEA.
@@ -15,5 +19,22 @@ public class ConfigLoader {
         this.customConfig = new Config(name, plugin);
         customConfig.saveDefaultConfig();
         this.config = customConfig.getConfig();
+        if(config.getString("DoNotChange") == null) {
+            config.set("DoNotChange", plugin.getDescription().getVersion());
+            customConfig.saveConfig(config);
+        } else {
+            if (!config.getString("DoNotChange").equals(plugin.getDescription().getVersion())) createBackup(name);
+        }
+    }
+
+    private void createBackup(String name) {
+        General.sendColoredMessage(plugin, "Found outdated config " + name + ". Creating a backup and then saving the new one!", ChatColor.RED);
+        File file = new File(plugin.getDataFolder(), name);
+        if(file.renameTo(new File(plugin.getDataFolder(), name.split("\\.")[0] + "_old" + "." + name.split("\\.")[1]))) {
+            customConfig.saveDefaultConfig();
+            this.config = customConfig.getConfig();
+            config.set("DoNotChange", plugin.getDescription().getVersion());
+            customConfig.saveConfig(config);
+        }
     }
 }
