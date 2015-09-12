544ed586dd794c0c928b9df3c41d4583d789c31b
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-09-12 20:53:21 +0200

    Added ShapedRecipe Parsing

--- src/com/j0ach1mmall3/jlib/methods/Parsing.java
+++ src/com/j0ach1mmall3/jlib/methods/Parsing.java
@@ -1,6 +1,12 @@
 package com.j0ach1mmall3.jlib.methods;
 
+import com.j0ach1mmall3.jlib.inventory.CustomItem;
 import org.bukkit.Material;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.material.MaterialData;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Parsing {
 	public static int parseString(String s){
@@ -31,4 +37,24 @@ public class Parsing {
 		if(item.endsWith(":")) return 0;
 		return Integer.valueOf(item.split(":")[1]);
 	}
+
+    @SuppressWarnings("deprecation")
+    public static ShapedRecipe parseShapedRecipe(List<String> recipe, CustomItem result) {
+        List<Character> chars = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i');
+        if(recipe.size() < 3) {
+            for(int a=0;a<3-recipe.size();a++) recipe.add("0 0 0");
+        }
+        ShapedRecipe shapedRecipe = new ShapedRecipe(result);
+        shapedRecipe.shape("abc", "def", "ghi");
+        int e=0;
+        for(int a=0;a<recipe.size();a++) {
+            String[] splitted = recipe.get(a).split(" ");
+            for(int i=0;i<splitted.length;i++) {
+                MaterialData data = new MaterialData(parseMaterial(splitted[i]), (byte) parseData(splitted[i]));
+                if(data.getItemType() != Material.AIR) shapedRecipe.setIngredient(chars.get(e), data);
+                e++;
+            }
+        }
+        return shapedRecipe;
+    }
 }
