18285366c16c5614af76b0729168b864b71fca6a
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-10-09 17:30:46 +0200

    Fixes

--- src/com/j0ach1mmall3/jlib/inventory/AsteriskItem.java
+++ src/com/j0ach1mmall3/jlib/inventory/AsteriskItem.java
@@ -1,5 +1,6 @@
 package com.j0ach1mmall3.jlib.inventory;
 
+import com.j0ach1mmall3.jlib.methods.Parsing;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -14,6 +15,28 @@ public class AsteriskItem {
         this.id = id;
         this.data = data;
         this.all = all;
+    }
+
+    public AsteriskItem(String item) {
+        if(!item.contains(":")) {
+            this.id = Parsing.parseString(item);
+            this.data = 0;
+            this.all = false;
+        }
+        if(item.startsWith(":") || item.endsWith(":")) {
+            this.id = Parsing.parseString(item.replace(":", ""));
+            this.data = 0;
+            this.all = false;
+        }
+        String[] splitted = item.split(":");
+        if(splitted[1].equalsIgnoreCase("*")) {
+            this.id = Parsing.parseString(splitted[0]);
+            this.data = 0;
+            this.all = true;
+        }
+        this.id = Parsing.parseString(splitted[0]);
+        this.data = Parsing.parseString(splitted[1]);
+        this.all = false;
     }
 
     public int getId() {
