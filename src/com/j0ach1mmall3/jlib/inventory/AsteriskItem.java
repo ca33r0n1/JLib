d0de2cdd27f0f88f9f34c0a748f0484b7bba6aac
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-11-04 13:32:05 +0100

    Updated AsteriskItem

--- src/com/j0ach1mmall3/jlib/inventory/AsteriskItem.java
+++ src/com/j0ach1mmall3/jlib/inventory/AsteriskItem.java
@@ -9,57 +9,60 @@ import org.bukkit.material.MaterialData;
  * Created by j0ach1mmall3 on 17:13 9/10/2015 using IntelliJ IDEA.
  */
 public class AsteriskItem {
-    private final int id;
-    private final int data;
-    private boolean all;
+    private final MaterialData data;
+    private final boolean all;
 
-    public AsteriskItem(int id, int data, boolean all) {
-        this.id = id;
-        this.data = data;
+    @SuppressWarnings("deprecation")
+    public AsteriskItem(int id, byte data, boolean all) {
+        this.data = new MaterialData(id, data);
         this.all = all;
     }
 
-    @SuppressWarnings("deprecation")
     public AsteriskItem(MaterialData data) {
-        this.id = data.getItemTypeId();
-        this.data = data.getData();
+        this.data = data;
+        this.all = false;
     }
 
+    @SuppressWarnings("deprecation")
     public AsteriskItem(String item) {
         if(!item.contains(":")) {
-            this.id = Parsing.parseInt(item);
-            this.data = 0;
+            this.data = new MaterialData(Parsing.parseInt(item), (byte) 0);
             this.all = false;
             return;
         }
         if(item.startsWith(":") || item.endsWith(":")) {
-            this.id = Parsing.parseInt(item.replace(":", ""));
-            this.data = 0;
+            this.data = new MaterialData(Parsing.parseInt(item.replace(":", "")), (byte) 0);
             this.all = false;
             return;
         }
         String[] splitted = item.split(":");
         if(splitted[1].equals("*")) {
-            this.id = Parsing.parseInt(splitted[0]);
-            this.data = 0;
+            this.data = new MaterialData(Parsing.parseInt(splitted[0]), (byte) 0);
             this.all = true;
             return;
         }
-        this.id = Parsing.parseInt(splitted[0]);
-        this.data = Parsing.parseInt(splitted[1]);
+        this.data = new MaterialData(Parsing.parseInt(splitted[0]), Parsing.parseByte(splitted[1]));
         this.all = false;
     }
 
     @SuppressWarnings("deprecation")
     public ItemStack getItem() {
-        return new ItemStack(this.id, 1, (short) this.data);
+        return new ItemStack(this.data.getItemType(), 1, this.data.getData());
     }
 
+    @SuppressWarnings("deprecation")
+    @Deprecated
     public int getId() {
-        return this.id;
+        return this.data.getItemTypeId();
     }
 
-    public int getData() {
+    @SuppressWarnings("deprecation")
+    @Deprecated
+    public byte getData() {
+        return this.data.getData();
+    }
+
+    public MaterialData getMaterialData() {
         return this.data;
     }
 
@@ -67,22 +70,22 @@ public class AsteriskItem {
         return this.all;
     }
 
-    @SuppressWarnings("deprecation")
     public boolean isItem(ItemStack is) {
-        return is.getTypeId() == this.id && (this.all || is.getDurability() == this.data);
+        return isItem(is.getData());
     }
 
     @SuppressWarnings("deprecation")
     public boolean isItem(Block b) {
-        return b.getTypeId() == this.id && (this.all || b.getData() == this.data);
+        return isItem(b.getTypeId(), b.getData());
     }
 
     @SuppressWarnings("deprecation")
     public boolean isItem(MaterialData data) {
-        return data.getItemTypeId() == this.id && (this.all || data.getData() == this.data);
+        return isItem(data.getItemTypeId(), data.getData());
     }
 
-    public boolean isItem(int id, int data) {
-        return id == this.id && (this.all || data == this.data);
+    @SuppressWarnings("deprecation")
+    public boolean isItem(int id, byte data) {
+        return id == this.data.getItemTypeId() && (this.all || data == this.data.getData());
     }
 }
