6032f70ef6bfa210a2f1c8c1649f9bb6dcec4e52
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-11-04 13:24:05 +0100

    Woops testing zone was still used :o

--- src/com/j0ach1mmall3/jlib/JoinListener.java
+++ src/com/j0ach1mmall3/jlib/JoinListener.java
@@ -1,7 +1,5 @@
 package com.j0ach1mmall3.jlib;
 
-import com.j0ach1mmall3.jlib.storage.serialization.SerializedInventory;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,13 +17,6 @@ public class JoinListener implements Listener {
     @SuppressWarnings({"EmptyMethod", "UnusedParameters"})
     @EventHandler
     public void onJoin(final PlayerJoinEvent e) {
-        final SerializedInventory inventory = new SerializedInventory(e.getPlayer().getInventory());
-        Bukkit.broadcastMessage(inventory.getString());
-        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-            @Override
-            public void run() {
-                e.getPlayer().openInventory(inventory.getInventory());
-            }
-        }, 60L);
+        //Testing zone :o
     }
 }
