214a5911fa1454117000f710cbed3699eeae63f0
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-11-25 19:54:11 +0100

    UUIDFetcher updates

--- src/main/java/com/j0ach1mmall3/jlib/integration/UUIDFetcher.java
+++ src/main/java/com/j0ach1mmall3/jlib/integration/UUIDFetcher.java
@@ -28,30 +28,30 @@ public final class UUIDFetcher implements Callable<Map<String, UUID>> {
 
     public Map<String, UUID> call() throws Exception {
         Map<String, UUID> uuidMap = new HashMap<>();
-        int requests = (int) Math.ceil(names.size() / 100);
+        int requests = (int) Math.ceil(this.names.size() / 100);
         for (int i = 0; i < requests; i++) {
-            HttpURLConnection connection = createConnection();
-            String body = JSONArray.toJSONString(names.subList(i * 100, Math.min((i + 1) * 100, names.size())));
+            HttpURLConnection connection = this.createConnection();
+            String body = JSONArray.toJSONString(this.names.subList(i * 100, Math.min((i + 1) * 100, this.names.size())));
             OutputStream stream = connection.getOutputStream();
             stream.write(body.getBytes());
             stream.flush();
             stream.close();
-            JSONArray array = (JSONArray) jsonParser.parse(new InputStreamReader(connection.getInputStream()));
+            JSONArray array = (JSONArray) this.jsonParser.parse(new InputStreamReader(connection.getInputStream()));
             for (Object profile : array) {
                 JSONObject jsonProfile = (JSONObject) profile;
                 String id = (String) jsonProfile.get("id");
                 String name = (String) jsonProfile.get("name");
-                UUID uuid = UUIDFetcher.getUUID(id);
+                UUID uuid = this.getUUID(id);
                 uuidMap.put(name, uuid);
             }
-            if (rateLimiting && i != requests - 1) {
+            if (this.rateLimiting && i != requests - 1) {
                 Thread.sleep(100L);
             }
         }
         return uuidMap;
     }
 
-    private static HttpURLConnection createConnection() throws Exception {
+    private HttpURLConnection createConnection() throws Exception {
         URL url = new URL("https://api.mojang.com/profiles/minecraft");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
@@ -62,7 +62,7 @@ public final class UUIDFetcher implements Callable<Map<String, UUID>> {
         return connection;
     }
 
-    private static UUID getUUID(String id) {
+    private UUID getUUID(String id) {
         return UUID.fromString(id.substring(0, 8) + '-' + id.substring(8, 12) + '-' + id.substring(12, 16) + '-' + id.substring(16, 20) + '-' +id.substring(20, 32));
     }
 
