d5a4668b58ebbdda80d8d61e917fb3a30d029f04
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-11-29 18:07:23 +0100

    Added more CallbackHandlers

---
+++ src/main/java/com/j0ach1mmall3/jlib/storage/database/callbackhandlers/IntCallbackHandler.java
@@ -0,0 +1,9 @@
+package com.j0ach1mmall3.jlib.storage.database.callbackhandlers;
+
+/**
+ * @author j0ach1mmall3 (business.j0ach1mmall3@gmail.com)
+ * @since 29/11/15
+ */
+public interface IntCallbackHandler {
+    void callback(int i);
+}
