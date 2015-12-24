8bc1d4cabd25fd6378468bee5bbcd4b3db02d30c
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2015-12-24 16:14:22 +0100

    Placeholders fixes & additions

--- src/main/java/com/j0ach1mmall3/jlib/integration/Placeholders.java
+++ src/main/java/com/j0ach1mmall3/jlib/integration/Placeholders.java
@@ -44,40 +44,55 @@ public final class Placeholders {
      */
     private static String parseInternal(Player player, String message) {
         if(Bukkit.getBukkitVersion().split("\\-")[0].startsWith("1.2") || Bukkit.getBukkitVersion().split("\\-")[0].startsWith("1.3")) return ChatColor.translateAlternateColorCodes('&', message);
-        message = message.replace("%serverip%", Bukkit.getIp())
-                .replace("%motd%", Bukkit.getMotd())
+        message = message.replace("%motd%", Bukkit.getMotd())
                 .replace("%servername%", Bukkit.getName())
                 .replace("%online%", String.valueOf(Bukkit.getOnlinePlayers().size()))
+                .replace("%server_online%", String.valueOf(Bukkit.getOnlinePlayers().size()))
                 .replace("%max%", String.valueOf(Bukkit.getMaxPlayers()));
         if(player != null) {
             message = message.replace("%playername%", player.getName())
+                    .replace("%player_name%", player.getName())
                     .replace("%displayname%", player.getDisplayName())
+                    .replace("%player_displayname%", player.getDisplayName())
                     .replace("%health%", String.valueOf(player.getHealth()))
+                    .replace("%player_health%", String.valueOf(player.getHealth()))
                     .replace("%X%", String.valueOf(player.getLocation().getBlockX()))
+                    .replace("%player_x%", String.valueOf(player.getLocation().getBlockX()))
                     .replace("%Y%", String.valueOf(player.getLocation().getBlockY()))
+                    .replace("%player_y%", String.valueOf(player.getLocation().getBlockY()))
                     .replace("%Z%", String.valueOf(player.getLocation().getBlockZ()))
+                    .replace("%player_z%", String.valueOf(player.getLocation().getBlockZ()))
                     .replace("%world%", player.getWorld().getName())
+                    .replace("%player_world%", player.getWorld().getName())
                     .replace("%level%", String.valueOf(player.getLevel()))
+                    .replace("%player_level%", String.valueOf(player.getLevel()))
                     .replace("%exp%", String.valueOf(player.getExp()))
-                    .replace("%ip%", player.getAddress().toString());
+                    .replace("%player_exp%", String.valueOf(player.getExp()))
+                    .replace("%ip%", player.getAddress().toString())
+                    .replace("%player_ip%", player.getAddress().toString());
             PermissionHook permissionHook = new PermissionHook();
             if (permissionHook.isRegistered()) {
                 if(!Bukkit.getBukkitVersion().split("\\-")[0].equals("1.2.5")) {
                     if (permissionHook.getPermission().hasGroupSupport()) {
-                        message = message.replace("%group%", permissionHook.getPermission().getPrimaryGroup(player));
+                        message = message.replace("%group%", permissionHook.getPermission().getPrimaryGroup(player))
+                                .replace("%vault_rank%", permissionHook.getPermission().getPrimaryGroup(player));
                     }
                 } else {
-                    message = message.replace("%group%", permissionHook.getPermission().getPrimaryGroup(player));
+                    message = message.replace("%group%", permissionHook.getPermission().getPrimaryGroup(player))
+                            .replace("%vault_rank%", permissionHook.getPermission().getPrimaryGroup(player));
                 }
             }
             ChatHook chatHook = new ChatHook();
             if (chatHook.isRegistered()) {
-                message = message.replace("%prefix%", chatHook.getChat().getPlayerSuffix(player))
-                        .replace("%suffix%", chatHook.getChat().getPlayerSuffix(player));
+                message = message.replace("%prefix%", chatHook.getChat().getPlayerPrefix(player))
+                        .replace("%vault_prefix%", chatHook.getChat().getPlayerPrefix(player))
+                        .replace("%suffix%", chatHook.getChat().getPlayerSuffix(player))
+                        .replace("%vault_suffix%", chatHook.getChat().getPlayerSuffix(player));
             }
             EconomyHook economyHook = new EconomyHook();
             if (economyHook.isRegistered()) {
-                message = message.replace("%balance%", String.valueOf(economyHook.getEconomy().getBalance(player)));
+                message = message.replace("%balance%", String.valueOf(economyHook.getEconomy().getBalance(player)))
+                        .replace("%vaulteco_balance%", String.valueOf(economyHook.getEconomy().getBalance(player)));
             }
         }
         message = ChatColor.translateAlternateColorCodes('&', message);
